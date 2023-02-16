1df44feb7daffde65efb36dddd97f65e84623c79
Данил Клочков <danil.klo4kov2016@yandex.ru>
2023-02-16 15:46:57 +0500

    3

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -33,7 +33,18 @@ namespace WindowsFormsApp2
                 // Цикл в течение месяцев в году
                 for (int month = 1; month <= 12; month++)
                 {
+                    // Смотрим, будет ли в этом месяце 13-я пятница
+                    DateTime test_date = new DateTime(year, month, 13);
 
+                    // Если мы не достигли даты начала
+                    if (test_date < start_date) continue;
+
+                    // Если мы закончили дату окончания
+                    if (test_date > end_date) break;
+
+                    // Пятница
+                    if (test_date.DayOfWeek == DayOfWeek.Friday)
+                        listBox1.Items.Add(test_date.ToShortDateString());
                 }
             }
 
